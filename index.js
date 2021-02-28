944818bd7a2ec1072023e851d7da7faec4e5035a
Tim Flood <tflood9@gmail.com>
2021-02-28 13:29:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 4;
+let num4 = 15;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(3, 4, 5, 20);
